b73ec6f9169386595cb3972a559de3382698930e
Ayush Mishra <ayushmishraatwork@gmail.com>
2025-11-13 21:11:44 +0530

    Clean up userAuth.routes.js by removing comments
    Removed commented-out code for protected route.

--- backend/src/routes/userAuth.routes.js
+++ backend/src/routes/userAuth.routes.js
@@ -3,16 +3,13 @@ import {
   registerUser,
   loginUser,
 } from "../controllers/userAuth.controller.js";
-// import { protectUser } from "../middleware/auth.middleware.js"; // You'd use this for a 'get profile' route
+
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
-// Example of a protected route:
-// router.get("/profile", protectUser, (req, res) => {
-//   res.json(req.user);
-// });
+
 
 export default router;
